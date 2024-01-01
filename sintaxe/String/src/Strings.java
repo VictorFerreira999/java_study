b84849debe137d8edd9450974cf2718da9e67d96
Victor <victorferreira9990@gmail.com>
2024-01-01 14:50:48 -0300

    Dia 1

---
+++ sintaxe/String/src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        // Sua string de exemplo
+        String minhaString = "Exemplo de String";
+
+        // Obtém a substring do índice 5 ao 7
+        String parteDaString = minhaString.substring(5, 8);
+
+        // Imprime a substring
+        System.out.println("Substring: " + parteDaString);
+    }
+}
